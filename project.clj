4546f538ada6baa5bfa5e2393ea1cf78ce95fcdf
Shane Vandegrift <vandesm14@gmail.com>
2023-09-30 16:33:38 -0400

    fix lein project file

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject cooking "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A new way to look at cooking, baking, and the related arts."
+  :url "https://github.com/vandesm14/cooking"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"] [org.clojure/data.json "2.4.0"]]
-  :repl-options {:init-ns cooking.core})
+  :repl-options {:init-ns cooking.core}
+  :main cooking.core)
